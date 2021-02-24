b7368dbd44ec796accb7f2d468ada8e66c8a6c2a
AndryRaza <70998539+AndryRaza@users.noreply.github.com>
2021-02-24 15:31:49 +0400

    Message log middleware

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class Admin
 {
@@ -18,10 +19,13 @@ class Admin
     {
         $user = $request->user();
 
+
         if ($user && $user->role_id == 0) {
             return $next($request);
         }
         
+        Log::warning($user->nom . ' ' . $user->prenom . ' a tenté d\'accèder aux pages d\'administration.');
+
         auth::logout();
         return redirect('');
         die();
